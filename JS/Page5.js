36749d28cb4d6eb9bc5ddf7c524ea950a6c66c6a
Moneeeeey <mani.esmaili85@gmail.com>
2024-05-07 22:03:10 -0700

    kunde

--- JS/Page5.js
+++ JS/Page5.js
@@ -36,9 +36,6 @@ function draw() {
   background("#000000");
   
 
-  // Apply orbit control to rotate the canvas
-  //orbitControl();
-
   rotationX += (mouseY - pmouseY) * 0.01;
   rotationY += (mouseX - pmouseX) * 0.01;
 
@@ -46,17 +43,15 @@ function draw() {
   translate(earthPositionX, earthPositionY, earthPositionZ);
   rotateX(rotationX);
   rotateY(rotationY);
-  noStroke(); // Disable stroke for the sphere
-  //model(earth);
+  noStroke();
   sphere(100);
-  pop(); // Restore the previous transformation state
+  pop(); 
 
-  // Display text fixed on the canvas
-  push(); // Save the current transformation state
+  push(); 
   
    stroke('#fffff');
-   //strokeWeight(20);
-  translate(-width / 2 + 10, -height / 2 + 30); // Set position for text
+
+  translate(-width / 2 + 10, -height / 2 + 30); 
   strokeWeight(20);
   if(mouseX == pmouseX && mouseY == pmouseY){
     circle(mouseX,mouseY,1);
@@ -64,7 +59,7 @@ function draw() {
      line(mouseX,mouseY,pmouseX,pmouseY);
   }
   textSize(100);
-  text("The ", width / 2, height / 6);
+  text("The ", width / 2, height / 4.8);
   textLeading(90);
   text("is \nso \n small! ", width / 2, height / 1.6);
   pop(); 
